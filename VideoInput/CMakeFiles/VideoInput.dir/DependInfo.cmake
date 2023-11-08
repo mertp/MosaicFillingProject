35091016fa535b0c4b06f4cf597c75620d0bc96c
mertp <mertcpekdemir@gmail.com>
2023-11-08 22:01:12 +0300

    This commit is a mistake

---
+++ VideoInput/CMakeFiles/VideoInput.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mertp/Desktop/mosaicProject/source/MosaicFillingProject/VideoInput/VideoInput.cpp" "/home/mertp/Desktop/mosaicProject/source/MosaicFillingProject/VideoInput/CMakeFiles/VideoInput.dir/VideoInput.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/mertp/Documents/build"
+  "/home/mertp/Documents/opencv-4.7.0/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/core/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/flann/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/imgproc/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/intensity_transform/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/ml/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/phase_unwrapping/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/photo/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/plot/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/quality/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/reg/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/surface_matching/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/xphoto/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/dnn/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/dnn_superres/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/features2d/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/freetype/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/fuzzy/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/hfs/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/img_hash/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/imgcodecs/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/line_descriptor/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/saliency/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/text/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/videoio/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/calib3d/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/datasets/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/highgui/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/mcc/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/objdetect/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/rapid/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/rgbd/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/shape/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/structured_light/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/ts/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/video/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/videostab/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/wechat_qrcode/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/xfeatures2d/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/ximgproc/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/xobjdetect/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/aruco/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/bgsegm/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/bioinspired/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/ccalib/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/dnn_objdetect/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/dpm/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/face/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/gapi/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/optflow/include"
+  "/home/mertp/Documents/opencv-4.7.0/modules/stitching/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/superres/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/tracking/include"
+  "/home/mertp/Documents/opencv_contrib-4.x/modules/stereo/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
