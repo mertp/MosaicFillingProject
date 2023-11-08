64456cd248c766a412e7392f17d1a2d17bf9bd2b
mertp <mertcpekdemir@gmail.com>
2023-11-08 21:30:05 +0300

    edge detection with video input

--- VideoInput/VideoInput.cpp
+++ VideoInput/VideoInput.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-    cv::VideoCapture cap(-1); // Open the default camera (index 0)
+    cv::VideoCapture cap(0); // Open the default camera (index 0)
 
     if (!cap.isOpened()) {
         std::cerr << "Error opening the video source." << std::endl;
@@ -9,17 +9,23 @@ int main() {
     }
 
     cv::namedWindow("Video Input", cv::WINDOW_NORMAL);
+    cv::namedWindow("Edge Detection", cv::WINDOW_NORMAL);
 
     while (true) {
-        cv::Mat frame;
+        cv::Mat frame, edgeFrame;
         if (!cap.read(frame)) {
             std::cerr << "Error capturing frame." << std::endl;
             break;
         }
 
-        cv::imshow("Video Input", frame);
+        // Apply Canny edge detection to the frame
+        cv::cvtColor(frame, edgeFrame, cv::COLOR_BGR2GRAY); // Convert to grayscale
+        cv::Canny(edgeFrame, edgeFrame, 50, 150); // Apply Canny edge detection
 
-        if (cv::waitKey(50) == 27) { // Exit when the 'Esc' key is pressed
+        cv::imshow("Video Input", frame);
+        cv::imshow("Edge Detection", edgeFrame);
+
+        if (cv::waitKey(1) == 27) { // Exit when the 'Esc' key is pressed
             break;
         }
     }
