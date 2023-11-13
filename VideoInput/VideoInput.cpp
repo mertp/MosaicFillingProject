6561171cd416f30b77aedea98e210b1ac7fc3128
mertp <mertcpekdemir@gmail.com>
2023-11-13 17:16:00 +0300

    bird-view perspective transformation and color masking

--- VideoInput/VideoInput.cpp
+++ VideoInput/VideoInput.cpp
@@ -1,4 +1,17 @@
-#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+
+void colorDetection(const cv::Mat& input, const cv::Scalar& lowerBound, const cv::Scalar& upperBound) {
+    cv::Mat hsv;
+    cv::cvtColor(input, hsv, cv::COLOR_BGR2HSV);
+
+    cv::Mat mask;
+    cv::inRange(hsv, lowerBound, upperBound, mask);
+
+    cv::imshow("Color Mask", mask);
+}
 
 int main() {
     cv::VideoCapture cap(0); // Open the default camera (index 0)
@@ -9,21 +22,41 @@ int main() {
     }
 
     cv::namedWindow("Video Input", cv::WINDOW_NORMAL);
-    cv::namedWindow("Edge Detection", cv::WINDOW_NORMAL);
+    cv::namedWindow("Bird's Eye View", cv::WINDOW_NORMAL);
+    cv::namedWindow("Color Mask", cv::WINDOW_NORMAL);
+
+    // Define source points (quadrilateral in the input image)
+    std::vector<cv::Point2f> srcPoints;
+    srcPoints.push_back(cv::Point2f(150, 100));  // Top-left
+    srcPoints.push_back(cv::Point2f(400, 100));  // Top-right
+    srcPoints.push_back(cv::Point2f(550, 300));  // Bottom-right
+    srcPoints.push_back(cv::Point2f(50, 300));   // Bottom-left
+
+    // Define destination points (rectangle in the output image)
+    std::vector<cv::Point2f> dstPoints;
+    dstPoints.push_back(cv::Point2f(100, 0));    // Top-left
+    dstPoints.push_back(cv::Point2f(500, 0));    // Top-right
+    dstPoints.push_back(cv::Point2f(500, 300));  // Bottom-right
+    dstPoints.push_back(cv::Point2f(100, 300));  // Bottom-left
+
+    cv::Mat perspectiveMatrix = cv::getPerspectiveTransform(srcPoints, dstPoints);
 
     while (true) {
-        cv::Mat frame, edgeFrame;
+        cv::Mat frame, birdseye;
         if (!cap.read(frame)) {
             std::cerr << "Error capturing frame." << std::endl;
             break;
         }
 
-        // Apply Canny edge detection to the frame
-        cv::cvtColor(frame, edgeFrame, cv::COLOR_BGR2GRAY); // Convert to grayscale
-        cv::Canny(edgeFrame, edgeFrame, 50, 150); // Apply Canny edge detection
+        // Apply perspective transformation to get bird's-eye view
+        cv::warpPerspective(frame, birdseye, perspectiveMatrix, frame.size());
+
+        cv::Scalar lowerBlue = cv::Scalar(0, 100, 100); // Adjust as needed
+        cv::Scalar upperBlue = cv::Scalar(30, 255, 255); // Adjust as needed
+        colorDetection(birdseye, lowerBlue, upperBlue);
 
         cv::imshow("Video Input", frame);
-        cv::imshow("Edge Detection", edgeFrame);
+        cv::imshow("Bird's Eye View", birdseye);
 
         if (cv::waitKey(1) == 27) { // Exit when the 'Esc' key is pressed
             break;
