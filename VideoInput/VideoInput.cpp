36c59f75fa14cec7cf4951ede3fab03027018781
mertp <mertcpekdemir@gmail.com>
2023-11-18 16:36:35 +0300

    multiple color detection with boundaries

--- VideoInput/VideoInput.cpp
+++ VideoInput/VideoInput.cpp
@@ -3,25 +3,63 @@
 #include "opencv2/highgui.hpp"
 #include <iostream>
 
-void colorDetection(const cv::Mat& input, const cv::Scalar& lowerBound, const cv::Scalar& upperBound, double& colorPercentage) {
+class Tile
+{
+public:
+    cv::Rect boundingBox;
+    double colorPercentage;
+
+    Tile(const cv::Rect &box) : boundingBox(box), colorPercentage(0.0) {}
+
+    // Function to calculate color percentage for the tile
+    void calculateColorPercentage(const cv::Mat &birdseye, const cv::Scalar &lowerBound, const cv::Scalar &upperBound)
+    {
+        cv::Mat mask;
+        cv::Mat hsv;
+        cv::cvtColor(birdseye(boundingBox), hsv, cv::COLOR_BGR2HSV);
+        cv::inRange(hsv, lowerBound, upperBound, mask);
+
+        int totalPixels = mask.rows * mask.cols;
+        int colorPixels = cv::countNonZero(mask);
+        colorPercentage = (static_cast<double>(colorPixels) / totalPixels) * 100.0;
+    }
+};
+
+void colorDetection(const cv::Mat &input, const cv::Scalar &lowerBound, const cv::Scalar &upperBound, std::vector<Tile> &tiles)
+{
     cv::Mat hsv;
     cv::cvtColor(input, hsv, cv::COLOR_BGR2HSV);
 
     cv::Mat mask;
     cv::inRange(hsv, lowerBound, upperBound, mask);
 
-    cv::imshow("Color Mask", mask);
+    // Find contours in the mask
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
-    // Calculate color percentage
-    int totalPixels = input.rows * input.cols;
-    int colorPixels = cv::countNonZero(mask);
-    colorPercentage = (static_cast<double>(colorPixels) / totalPixels) * 100.0;
+    // Process each detected region
+    for (const auto &contour : contours)
+    {
+        // Calculate bounding box for each contour
+        cv::Rect boundingBox = cv::boundingRect(contour);
+
+        // Create a Tile object for each detected tile
+        Tile tile(boundingBox);
+
+        // Calculate color percentage for the tile
+        tile.calculateColorPercentage(input, lowerBound, upperBound);
+
+        // Add the tile to the vector
+        tiles.push_back(tile);
+    }
 }
 
-int main() {
+int main()
+{
     cv::VideoCapture cap(0); // Open the default camera (index 0)
 
-    if (!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         std::cerr << "Error opening the video source." << std::endl;
         return -1;
     }
@@ -32,23 +70,25 @@ int main() {
 
     // Define source points (quadrilateral in the input image)
     std::vector<cv::Point2f> srcPoints;
-    srcPoints.push_back(cv::Point2f(150, 100));  // Top-left
-    srcPoints.push_back(cv::Point2f(400, 100));  // Top-right
-    srcPoints.push_back(cv::Point2f(550, 300));  // Bottom-right
-    srcPoints.push_back(cv::Point2f(50, 300));   // Bottom-left
+    srcPoints.push_back(cv::Point2f(150, 10));  // Top-left
+    srcPoints.push_back(cv::Point2f(400, 10));  // Top-right
+    srcPoints.push_back(cv::Point2f(550, 200)); // Bottom-right
+    srcPoints.push_back(cv::Point2f(50, 200));  // Bottom-left
 
     // Define destination points (rectangle in the output image)
     std::vector<cv::Point2f> dstPoints;
-    dstPoints.push_back(cv::Point2f(100, 0));    // Top-left
-    dstPoints.push_back(cv::Point2f(500, 0));    // Top-right
-    dstPoints.push_back(cv::Point2f(500, 300));  // Bottom-right
-    dstPoints.push_back(cv::Point2f(100, 300));  // Bottom-left
+    dstPoints.push_back(cv::Point2f(100, 0));   // Top-left
+    dstPoints.push_back(cv::Point2f(500, 0));   // Top-right
+    dstPoints.push_back(cv::Point2f(500, 300)); // Bottom-right
+    dstPoints.push_back(cv::Point2f(100, 300)); // Bottom-left
 
     cv::Mat perspectiveMatrix = cv::getPerspectiveTransform(srcPoints, dstPoints);
 
-    while (true) {
+    while (true)
+    {
         cv::Mat frame, birdseye;
-        if (!cap.read(frame)) {
+        if (!cap.read(frame))
+        {
             std::cerr << "Error capturing frame." << std::endl;
             break;
         }
@@ -56,18 +96,46 @@ int main() {
         // Apply perspective transformation to get bird's-eye view
         cv::warpPerspective(frame, birdseye, perspectiveMatrix, frame.size());
 
-        cv::Scalar lowerBlue = cv::Scalar(0, 0, 0); // Adjust as needed
-        cv::Scalar upperBlue = cv::Scalar(105, 105, 105); // Adjust as needed
-        double colorPercentage;
-        colorDetection(frame, lowerBlue, upperBlue,colorPercentage);
+        cv::Scalar lowerRed1 = cv::Scalar(0, 100, 100);  // Lower bound for red (first range)
+        cv::Scalar upperRed1 = cv::Scalar(10, 255, 255); // Upper bound for red (first range)
 
-         std::string text = "Black color Percentage: " + std::to_string(colorPercentage) + "%";
-        cv::putText(frame, text, cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 0), 2, cv::LINE_AA);
+        cv::Scalar lowerRed2 = cv::Scalar(160, 100, 100); // Lower bound for red (second range)
+        cv::Scalar upperRed2 = cv::Scalar(180, 255, 255); // Upper bound for red (second range)
+
+        cv::Scalar lowerBlue1 = cv::Scalar(100, 100, 100);
+        cv::Scalar upperBlue1 = cv::Scalar(120, 255, 255);
+        std::vector<Tile> redTiles, blueTiles;
+        double colorPercentage;
+        colorDetection(frame, lowerRed1, upperRed1, redTiles);
+        colorDetection(frame, lowerBlue1, upperBlue1, blueTiles);
+        for (const auto &tile : redTiles)
+        {
+            // Draw bounding box on the original frame
+            cv::rectangle(frame, tile.boundingBox, cv::Scalar(0, 0, 255), 2); // Red color for bounding box
+
+            // Display color percentage on the frame
+            std::string text = "Red Percentage: " + std::to_string(tile.colorPercentage) + "%";
+
+            // Print the color percentage to the console
+            std::cout << text << std::endl;
+        }
+        for (const auto &tile : blueTiles)
+        {
+            // Draw bounding box on the original frame
+            cv::rectangle(frame, tile.boundingBox, cv::Scalar(255, 0, 0), 2); // Blue color for bounding box
+
+            // Display color percentage on the frame
+            std::string text = "Blue Percentage: " + std::to_string(tile.colorPercentage) + "%";
+
+            // Print the color percentage to the console
+            std::cout << text << std::endl;
+        }
 
         cv::imshow("Video Input", frame);
-        //cv::imshow("Bird's Eye View", birdseye);
+        cv::imshow("Bird's Eye View", birdseye);
 
-        if (cv::waitKey(1) == 27) { // Exit when the 'Esc' key is pressed
+        if (cv::waitKey(1) == 27)
+        { // Exit when the 'Esc' key is pressed
             break;
         }
     }
