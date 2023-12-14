1ed5ba083bb5734158fd0f2d4184910c4a006c1f
mervebulbul <mrvbulbul2000@gmail.com>
2023-12-14 20:20:29 +0300

    birdview has added with orb algorithm.

---
+++ MosaicFillingProject.cpp
@@ -0,0 +1,84 @@
+﻿#include "MosaicFillingProject.h"
+#include "birdview.h"
+#include "colordetection.h"
+
+cv::Scalar lowerRed1 = cv::Scalar(0, 100, 100);  // Lower bound for red (first range)
+cv::Scalar upperRed1 = cv::Scalar(10, 255, 255); // Upper bound for red (first range)
+
+cv::Scalar lowerRed2 = cv::Scalar(160, 100, 100); // Lower bound for red (second range)
+cv::Scalar upperRed2 = cv::Scalar(180, 255, 255); // Upper bound for red (second range)
+
+cv::Scalar lowerBlue1 = cv::Scalar(100, 100, 100);
+cv::Scalar upperBlue1 = cv::Scalar(120, 255, 255);
+
+int main() {
+    birdview view;
+    colordetection detect;
+
+    string refImagePath = "/Users/Hp/Desktop/MosaicFillingProject/img4.jpg";
+    Mat imReference = imread(refImagePath);
+    if (imReference.empty()) {
+        cerr << "Error: Reference image cannot be loaded." << endl;
+        return -1;
+    }
+
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cerr << "Error: Camera cannot be opened." << endl;
+        return -1;
+    }
+
+    Mat frame, imReg, h;
+
+    while (true) {
+        cap >> frame;
+
+        if (frame.empty()) {
+            cerr << "End of video stream." << endl;
+            break;
+        }
+
+
+        std::vector<Tile> redTiles, blueTiles;
+        double colorPercentage;
+        detect.colorDetection(frame, lowerRed1, upperRed1, redTiles);
+        detect.colorDetection(frame, lowerBlue1, upperBlue1, blueTiles);
+        for (const auto& tile : redTiles)
+        {
+            // Draw bounding box on the original frame
+            cv::rectangle(frame, tile.boundingBox, cv::Scalar(0, 0, 255), 2); // Red color for bounding box
+
+            // Display color percentage on the frame
+            std::string text = "Red Percentage: " + std::to_string(tile.colorPercentage) + "%";
+
+            // Print the color percentage to the console
+            std::cout << text << std::endl;
+        }
+        for (const auto& tile : blueTiles)
+        {
+            // Draw bounding box on the original frame
+            cv::rectangle(frame, tile.boundingBox, cv::Scalar(255, 0, 0), 2); // Blue color for bounding box
+
+            // Display color percentage on the frame
+            std::string text = "Blue Percentage: " + std::to_string(tile.colorPercentage) + "%";
+
+            // Print the color percentage to the console
+            std::cout << text << std::endl;
+        }
+
+        cv::imshow("Video Input", frame);
+
+        view.alignImages(frame, imReference, imReg, h);
+
+        // imshow kullanarak iki pencereyi açın
+        imshow("Aligned Frame", imReg);
+
+        if (waitKey(1) == 27) // Break the loop if Esc is pressed
+            break;
+    }
+
+    cap.release();
+    destroyAllWindows();
+
+    return 0;
+}
---
+++ birdview.cpp
@@ -0,0 +1,55 @@
+#include "birdview.h"
+
+void birdview::alignImages(cv::Mat& im1, cv::Mat& im2, cv::Mat& im1Reg, cv::Mat& h)
+{
+
+    cv::Mat im1Gray, im2Gray;
+    cvtColor(im1, im1Gray, cv::COLOR_BGR2GRAY);
+    cvtColor(im2, im2Gray, cv::COLOR_BGR2GRAY);
+
+    // Feature extraction
+    std::vector<cv::KeyPoint> keypoints1, keypoints2;
+    cv::Mat descriptors1, descriptors2;
+    Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
+    orb->detectAndCompute(im1Gray, Mat(), keypoints1, descriptors1);
+    orb->detectAndCompute(im2Gray, Mat(), keypoints2, descriptors2);
+
+    // Match features.
+    std::vector<DMatch> matches;
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+    matcher->match(descriptors1, descriptors2, matches, Mat());
+
+    vector<DMatch> goodMatches;
+    for (size_t i = 0; i < matches.size(); i++)
+    {
+        if (matches[i].distance < DISTANCE_THRESHOLD)
+        {
+            goodMatches.push_back(matches[i]);
+        }
+    }
+
+    std::sort(matches.begin(), matches.end());
+
+    const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
+    matches.erase(matches.begin() + numGoodMatches, matches.end());
+ 
+   
+     Mat imMatches;
+     drawMatches(im1, keypoints1, im2, keypoints2, matches, imMatches);
+     imshow("Matches", imMatches);
+    
+     // Extract location of good matches
+    std::vector<Point2f> points1, points2;
+
+    for (size_t i = 0; i < matches.size(); i++)
+    {
+        points1.push_back(keypoints1[matches[i].queryIdx].pt);
+        points2.push_back(keypoints2[matches[i].trainIdx].pt);
+    }
+
+    // RANSAC(rastgele örnek konsensüsü) homografi matrisi
+    h = findHomography(points1, points2, RANSAC);
+
+    // Use homography to warp image
+    warpPerspective(im1, im1Reg, h, im2.size());
+}
